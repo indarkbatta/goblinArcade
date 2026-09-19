e910812e345a5e49b08617a4f069087edada3f63
indarkbatta <indarkbatta@gmail.com>
2026-09-19 17:51:24 +0200

    Bump GoblinArcade to 0.2.0

--- GoblinArcade/Core.lua
+++ GoblinArcade/Core.lua
@@ -4,7 +4,7 @@ GA = GA or {}
 _G.GoblinArcade = GA
 
 GA.name = "GoblinArcade"
-GA.version = "0.1.1"
+GA.version = "0.2.0"
 
 local eventFrame = CreateFrame("Frame")
 eventFrame:RegisterEvent("PLAYER_LOGIN")
